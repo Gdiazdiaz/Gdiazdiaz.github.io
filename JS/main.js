1192d20cab8570c93c266c20365fd27b9c19337f
Glenda Diaz <glendadiazah@outlook.com>
2022-11-15 11:12:02 -0600

    Add js comands to close menu

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,18 @@
 const menuButton = document.querySelector('.menu-button-container');
+const navbar = document.querySelector('#menu-display');
+const closingButton = document.querySelector('.cancel-button');
 
 
 function menuToggler() {
-    const navbar = document.querySelector('#menu-display');
     navbar.style.display = 'block';
     menuButton.style.display = 'none';
   }
 
+function menuClosing() {
+  navbar.style.display = 'none';
+  menuButton.style.display = 'block';
+}
   
 
 menuButton.addEventListener('click', menuToggler);
+closingButton.addEventListener('click', menuClosing);
