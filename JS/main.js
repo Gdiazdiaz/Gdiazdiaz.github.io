b3035e25ab9720e3f50fba9de8b2872b55bbc40b
Glenda Diaz <glendadiazah@outlook.com>
2022-11-15 12:21:26 -0600

    Fix linter issues

--- JS/main.js
+++ JS/main.js
@@ -3,19 +3,16 @@ const navbar = document.querySelector('#menu-display');
 const closingButton = document.querySelector('.cancel-button');
 const itemshow = document.querySelectorAll('.menu-item');
 
-
 function menuToggler() {
-    navbar.style.display = 'block';
-    menuButton.style.display = 'none';
-  }
+  navbar.style.display = 'block';
+  menuButton.style.display = 'none';
+}
 
 function menuClosing() {
   navbar.style.display = 'none';
   menuButton.style.display = 'block';
 }
-  
 
 menuButton.addEventListener('click', menuToggler);
 closingButton.addEventListener('click', menuClosing);
-itemshow.forEach(element => element.addEventListener('click', menuClosing));
-console.log(itemshow)
+itemshow.forEach((element) => element.addEventListener('click', menuClosing));
