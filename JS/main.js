0ae57b41a92868c7ad7c5bf19e5b5f8ee1c3bcbc
Glenda Diaz <glendadiazah@outlook.com>
2022-11-15 16:49:27 -0600

    Fix size issues in desktop version

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,19 @@ const menuButton = document.querySelector('.menu-button-container');
 const navbar = document.querySelector('#menu-display');
 const closingButton = document.querySelector('.cancel-button');
 const itemshow = document.querySelectorAll('.menu-item');
+const navb = document.querySelector('.nav-bar')
 
 function menuToggler() {
   navbar.style.display = 'flex';
   menuButton.style.display = 'none';
+  navb.style.position = 'fixed';
+
 }
 
 function menuClosing() {
   navbar.style.display = 'none';
   menuButton.style.display = 'block';
+  navb.style.position = 'absolute';
 }
 
 menuButton.addEventListener('click', menuToggler);
